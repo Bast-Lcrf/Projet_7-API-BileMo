916b7926857b7523b8c6baf8a450ecdd9efe9d11
Bastien Lecerf <lecerf.bastien@me.com>
2023-01-12 20:33:55 +0100

    Some new features

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,30 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Product;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+        $date = new \DateTime('Europe/Paris');
+        $marques = ["Apple", "Samsung", "Huawei", "LG"];
+        $os = ['IOS', 'Android'];
+        $price = [500, 1000, 1500];
+
+        for ($i = 0; $i < 20; $i++) { 
+            $product = new Product();
+            $product->setName('mobile nº: ' . $i);
+            $product->setBrand($marques[array_rand($marques)]);
+            $product->setReleaseDate($date);
+            $product->setOperatingSystem($os[array_rand($os)]);
+            $product->setPrice($price[array_rand($price)]);
+            $manager->persist($product);
+        }
 
         $manager->flush();
     }

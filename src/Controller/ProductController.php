3c9bf85511c4a387f6ab78443e12c21e426da4c4
Bastien Lecerf <lecerf.bastien@me.com>
2023-01-14 12:05:58 +0100

    DELETE product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -4,22 +4,33 @@ namespace App\Controller;
 
 use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use JMS\Serializer\SerializerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 class ProductController extends AbstractController
 {
     /**
-     * Cette méthode nous permet de récupérer la liste de tous les produits
+     * Cette méthode nous permet de récupérer la liste de tous les produits,
+     * Pagination 5 produits par pages, et mise en cache des éléments
      *
-     * @return void
+     * @param Request $request
+     * @param ProductRepository $productRepository
+     * @param SerializerInterface $serializer
+     * @param TagAwareCacheInterface $cache
+     * @return JsonResponse
      */
     #[Route('/api/products', name: 'product', methods: ['GET'])] 
     public function getAllProducts(
@@ -44,13 +55,38 @@ class ProductController extends AbstractController
 
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
     }
-
-    #[Route('/api/products/{id}', name: 'detailProduct', methods: ['GET'])] 
+   
+    /**
+     * Cette méthode nous permet de récupérer le detail d'un produit via son id
+     *
+     * @param  Product $product
+     * @param  SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    #[Route('/api/products/{id}', name: 'detailProduct', methods: ['GET'])]
     public function getDetailProduct(Product $product, SerializerInterface $serializer): JsonResponse
     {
         $context = SerializationContext::create()->setGroups('getProducts');
         $jsonProduct = $serializer->serialize($product, 'json', $context);
         return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
     }
+
     
+    /**
+     * Cette méthode nous permet de supprimer un produit par rapport a son id
+     *
+     * @param  mixed $product
+     * @param  mixed $em
+     * @param  mixed $cache
+     * @return JsonResponse
+     */
+    #[Route('/api/products/{id}', name: 'deleteProduct', methods: ['DELETE'])]
+    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour supprimer un produit')]
+    public function deleteProduct(Product $product, EntityManagerInterface $em, TagAwareCacheInterface $cache): JsonResponse
+    {
+        $cache->invalidateTags(['productCache']);
+        $em->remove($product);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
 }
